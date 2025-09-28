1c6fa7d1ff049985ff7871358e1c44aff9e5790c
MehfCodes <mrmohammadehf@gmail.com>
2025-09-28 17:47:23 +0330

    other changes for adding Guid Id propery to User Model and its relations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,11 +1,12 @@
 using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SimpleBlog.Models.Domain;
 
 namespace SimpleBlog.Data;
 
-public class AppDbContext :  IdentityDbContext<User>
+public class AppDbContext :  IdentityDbContext<User, IdentityRole<Guid>, Guid>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
--- Models/Domain/User.cs
+++ Models/Domain/User.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace SimpleBlog.Models.Domain;
 
-public class User : IdentityUser
+public class User : IdentityUser<Guid>
 {
     public required string Username { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
