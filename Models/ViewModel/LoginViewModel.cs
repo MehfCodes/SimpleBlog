a89ae25afc457b46c3d7ea9331fc98d2559add1a
MehfCodes <mrmohammadehf@gmail.com>
2025-10-04 14:07:07 +0330

    define viewmodel for login and register

---
+++ Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.Models.ViewModel;
+
+public class LoginViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
---
+++ Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.Models.ViewModel;
+
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    public string? ConfirmPassword { get; set; }
+}
